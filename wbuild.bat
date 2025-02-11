6dbdc4cc61ddcbc49c2e69a176d29463ccbcdd0a
Jesse Sher <jebba272727@gmail.com>
2025-02-11 22:25:47 +1100

    tested lua

--- wbuild.bat
+++ wbuild.bat
@@ -1,1 +1,1 @@
-em++ -o build/index.html src/main.cpp src/Entity/Entity.cpp src/Game/Game.cpp -std=c++17 -Os -Wall ./llib/libraylib.a -I. -I include/ -L. -L llib/ -I"include/sol" -llua54 -s USE_GLFW=3 --shell-file build/shell.html -DPLATFORM_WEB
+em++ -o -v build/index.html src/main.cpp src/Entity/Entity.cpp src/Game/Game.cpp -std=c++17 -Os -Wall ./llib/libraylib.a -I. -I include/ -L. -L llib/ -I"include/sol" -llua54 -s USE_GLFW=3 --shell-file build/shell.html -DPLATFORM_WEB
